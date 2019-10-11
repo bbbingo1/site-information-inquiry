406e4c98191736cd7a7ce999e6329e0f9a17aa2c
bbbingo1 <846790344@qq.com>
2019-10-11 23:20:06 +0800

    feat：添加高德地图搜索插件，完善距离查询功能

--- src/components/maps/GaodeMap.vue
+++ src/components/maps/GaodeMap.vue
@@ -2,27 +2,28 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-06 21:25:37
- * @LastEditTime: 2019-10-10 22:59:34
+ * @LastEditTime: 2019-10-11 23:06:21
  * @LastEditors: Please set LastEditors
  -->
 <template>
   <div class="m-map">
-    <div class="search" v-show="showTrafficSearch">
+    <div class="search">
       <div style="display: inline-block; padding:10px;40px">
-        <el-input size="small" id="locationInput" :value="departureValue.lng+','+departureValue.lat+';'+destinationValue.lng+','+destinationValue.lat" disabled></el-input>
+        <el-input size="small" id="locationInput" :value="Number(departureValue.lng).toFixed(3)+','+Number(departureValue.lat).toFixed(3)+';'+Number(destinationValue.lng).toFixed(3)+','+Number(destinationValue.lat).toFixed(3)" disabled></el-input>
       </div>
       <div style="display: inline-block; padding:10px;40px">
         <el-input size="small" id="targetInput" v-model="destinationModel" placeholder="请输入目的地"></el-input>
       </div>
       <div style="display: inline-block; padding-right:10px;">
-        <el-button type="primary" icon="el-icon-search" @click="searchItems">测量距离</el-button>
+        <el-button type="primary" icon="el-icon-search" @click="searchItems('test')">测量距离</el-button>
       </div>
       <div style="display: inline-block;">
-        <el-button type="info" icon="el-icon-search" @click="searchItems">查看交通</el-button>
+        <el-button type="info" icon="el-icon-search" @click="searchItems('traffic')">查看交通</el-button>
       </div>
     </div>
     <div class="m-container">
       <div id="js-container" class="map" @mousewheel.stop>正在加载地图 ...</div>
+      <div id="js-panel" @mousewheel.stop></div>
     </div>
     <div class="m-result">
       <div id="js-result" class="result" @mousewheel.stop></div>
@@ -34,13 +35,14 @@
 <script>
 /* eslint-disable */
 import remoteLoad from "@/utils/remoteLoad.js";
+import { getTrafficInfo } from "@/api/gaodeMap.js";
 const MapKey = "da5fae8f5e10d86564f1c0803232c577";
 const MapCityName = "深圳";
 let map;
 export default {
   props: {
-    lat: String,
-    lng: String,
+    lat: String, //中心纬度
+    lng: String, //中心精度
     listMsg: Array
   },
   data() {
@@ -49,19 +51,17 @@ export default {
       AMap: null,
       listenObj: { tag: true }, //仿照watch利用对象访问器属性监听listMsg数据变更
       //trafficItems|以下
-      showTrafficSearch: false,
+      // showTrafficSearch: false,
       departureData: "",
       destinationModel: "",
-      departureValue: { lng: "", lat: "" }, //起点经纬度
-      destinationValue: { lng: "", lat: "" }, //终点经纬度
+      departureValue: { lng: 0, lat: 0 }, //起点经纬度
+      destinationValue: { lng: 0, lat: 0 }, //终点经纬度
       independentMarkerItem: {
+        placeSearch: null, //地点查询实例
         markers: [], //存放查询生成的标注点
         line: null,
         text: null
-      },
-      markers: [], //存放查询生成的标注点
-      line: null,
-      text: null
+      }
     };
   },
   computed: {
@@ -80,7 +80,6 @@ export default {
       // 加载MarkerList，loadUI的路径参数为模块名中 'ui/' 之后的部分
       let AMapUI = (this.AMapUI = window.AMapUI);
       let AMap = (this.AMap = window.AMap);
-      // let map;
       let that = this;
       //地图加载
       AMapUI.loadUI(
@@ -110,7 +109,7 @@ export default {
             getPosition: dataItem => {
               //返回数据项的经纬度，AMap.LngLat实例或者经纬度数组
               // return dataItem.position;
-              return [dataItem.location.lat, dataItem.location.lng];
+              return [dataItem.location.lng, dataItem.location.lat];
             },
             getInfoWindow: function(dataItem, context, recycledInfoWindow) {
               // var tpl = "<p><%- dataItem.id %>：<%- dataItem.desc %><p>";
@@ -212,7 +211,7 @@ export default {
 
           //监听Marker和ListElement上的点击，详见markerEvents，listElementEvents
           markerList.on("markerClick listElementClick", (event, record) => {
-            this.showTrafficSearch = true;
+            // this.showTrafficSearch = true;
             this.departureData = record.data;
             this.departureValue = {
               lng: record.data.location.lng,
@@ -233,7 +232,7 @@ export default {
             set(val) {
               this._tag = val;
               markerList.render(that.listMsg);
-              that.showTrafficSearch = false; //显示交通查询等相关项
+              // that.showTrafficSearch = false; //显示交通查询等相关项
               return val;
             }
           });
@@ -246,60 +245,138 @@ export default {
           // });
         }
       );
-    },
-    searchItems() {
-      //创建一个icon
-      var startIcon = new AMap.Icon({
-        // 图标尺寸
-        size: new AMap.Size(25, 34),
-        // 图标的取图地址
-        image:
-          "http://a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
-        // 图标所用图片大小
-        imageSize: new AMap.Size(135, 40),
-        // 图标取图偏移量
-        imageOffset: new AMap.Pixel(-9, -3)
-      });
-      // 创建一个 icon
-      var endIcon = new AMap.Icon({
-        size: new AMap.Size(25, 34),
-        image:
-          "http://a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
-        imageSize: new AMap.Size(135, 40),
-        imageOffset: new AMap.Pixel(-95, -3)
-      });
-      //移除之前的标注
-      window.map.remove(this.independentMarkerItem.markers);
-      // 新增两个标注点
-      var m1 = new AMap.Marker({
-        map: window.map,
-        draggable: true,
-        position: new AMap.LngLat(
-          this.departureValue.lat,
-          this.departureValue.lng
-        ),
-        icon: startIcon,
-        offset: new AMap.Pixel(-13, -30),
-        zIndex: 140
-      });
-      var m2 = new AMap.Marker({
-        map: window.map,
-        draggable: true,
-        position: new AMap.LngLat(116.387271, 39.922501),
-        icon: endIcon,
-        offset: new AMap.Pixel(-13, -30),
-        zIndex: 140
-      });
-      this.independentMarkerItem.markers.push(m1, m2);
-      window.map.setFitView();
 
-      this.computeDis.call(this,m1,m2);
-      m1.on("dragging", this.computeDis.bind(this,m1,m2));
-      m2.on("dragging", this.computeDis.bind(this,m1,m2));
+      //自动补全搜索（目的地）
+      AMap.service(["AMap.Autocomplete"], function() {
+        var autoOptions = {
+          // city: "深圳", //城市，默认全国
+          input: "targetInput" //使用联想输入的input的id
+        };
+        let autocomplete = new AMap.Autocomplete(autoOptions);
+        AMap.event.addListener(autocomplete, "select", e => {
+          //搜索目的地
+          that.searchPanel(e.poi.name);
+        });
+      });
+    },
+    /**
+     * @description:选定目的地后执行测量距离及查询交通状况等操作
+     * @param {String}
+     * @return:
+     */
+    searchItems(type = "test") {
+      if (
+        this.destinationValue.lat &&
+        this.destinationValue.lng &&
+        this.departureValue.lat &&
+        this.departureValue.lng
+      ) {
+        //创建一个icon
+        var startIcon = new AMap.Icon({
+          // 图标尺寸
+          size: new AMap.Size(25, 34),
+          // 图标的取图地址
+          image:
+            "http://a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+          // 图标所用图片大小
+          imageSize: new AMap.Size(135, 40),
+          // 图标取图偏移量
+          imageOffset: new AMap.Pixel(-9, -3)
+        });
+        // 创建一个 icon
+        var endIcon = new AMap.Icon({
+          size: new AMap.Size(25, 34),
+          image:
+            "http://a.amap.com/jsapi_demos/static/demo-center/icons/dir-marker.png",
+          imageSize: new AMap.Size(135, 40),
+          imageOffset: new AMap.Pixel(-95, -3)
+        });
+
+        //移除之前的标注
+        window.map.remove(this.independentMarkerItem.markers);
+
+        // 新增两个标注点
+        var m1 = new AMap.Marker({
+          map: window.map,
+          draggable: true,
+          position: new AMap.LngLat(
+            this.departureValue.lng,
+            this.departureValue.lat
+          ),
+          icon: startIcon,
+          offset: new AMap.Pixel(-13, -30),
+          zIndex: 140
+        });
+        var m2 = new AMap.Marker({
+          map: window.map,
+          draggable: true,
+          position: new AMap.LngLat(
+            this.destinationValue.lng,
+            this.destinationValue.lat
+          ),
+          icon: endIcon,
+          offset: new AMap.Pixel(-13, -30),
+          zIndex: 140
+        });
+        //把两个标注记录到data中，便于下次移除
+        this.independentMarkerItem.markers.push(m1, m2);
+        window.map.setFitView();
+
+        if (type == "test") {
+          //查询距离
+          this.computeDis.call(this, m1, m2);
+          m1.on("dragging", this.computeDis.bind(this, m1, m2));
+          m2.on("dragging", this.computeDis.bind(this, m1, m2));
+        } else if (type == "traffic") {
+          let rectangle =
+            Number(this.departureValue.lng)+','+Number(this.departureValue.lat)+';'+Number(this.destinationValue.lng)+','+Number(this.destinationValue.lat);
+          getTrafficInfo(MapKey, rectangle)
+            .then(response => {
+              console.log(response);
+            })
+            .catch(error => console.log(error));
+        }
+      } else {
+        this.$message({
+          type: "warning",
+          message: "请先选择一个起始地点!"
+        });
+      }
+    },
+    /**
+     * @description: 生成目的地查询列表并触发执行后续操作
+     * @param {String}
+     * @return:
+     */
+    searchPanel(content) {
+      AMap.service(["AMap.PlaceSearch"], () => {
+        if (!this.independentMarkerItem.placeSearch) {
+          //构造地点查询类
+          this.independentMarkerItem.placeSearch = new AMap.PlaceSearch({
+            pageSize: 5, // 单页显示结果条数
+            pageIndex: 1, // 页码
+            // city: "深圳", // 兴趣点城市
+            citylimit: true, //是否强制限制在设置的城市内搜索
+            map: window.map, // 展现结果的地图实例
+            panel: "js-panel", // 结果列表将在此容器中进行展示。
+            autoFitView: true // 是否自动调整地图视野使绘制的 Marker点都处于视口的可见范围
+          });
+          AMap.event.addListener(
+            this.independentMarkerItem.placeSearch,
+            "selectChanged",
+            e => {
+              this.destinationValue.lat = e.selected.data.location.lat;
+              this.destinationValue.lng = e.selected.data.location.lng;
+            }
+          );
+        }
+        //关键字查询
+        this.independentMarkerItem.placeSearch.search(content);
+      });
     },
     /**
      * @description: 传入两个参数，都是marker构造生成的实例//测量两点距离的函数
-     * @param {AMap.Marker,AMap.Marker} 
+     * @param {AMap.Marker,AMap.Marker}
      * @return: null
      */
     computeDis(m1, m2) {
@@ -316,10 +393,10 @@ export default {
           outlineColor: "white",
           path: path
         });
-      } else {
-        this.independentMarkerItem.line.setPath(path);
       }
+      this.independentMarkerItem.line.setPath(path);
       if (!this.independentMarkerItem.text) {
+        console.log(distance);
         this.independentMarkerItem.text = new AMap.Text({
           text: "两点相距" + distance + "米",
           position: textPos,
@@ -330,10 +407,10 @@ export default {
             "font-size": "16px"
           }
         });
-      } else {
-        this.independentMarkerItem.text.setText("两点相距" + distance + "米");
-        this.independentMarkerItem.text.setPosition(textPos);
       }
+      console.log(distance);
+      this.independentMarkerItem.text.setText("两点相距" + distance + "米");
+      this.independentMarkerItem.text.setPosition(textPos);
     }
   },
   async created() {
@@ -362,13 +439,25 @@ export default {
   margin-bottom: 20px;
 
   .m-container {
+    position: relative;
     padding: 20px;
     width: 70%;
     height: 100%;
     box-sizing: border-box;
     float: left;
   }
-
+  #js-panel {
+    position: absolute;
+    background-color: white;
+    max-height: 80%;
+    overflow-y: auto;
+    top: 10%;
+    left: 2%;
+    width: 24%;
+    opacity: 0.9;
+    // border: 1px solid #ccc;
+    border-radius: 2px;
+  }
   .map {
     display: inline-block;
     width: 100%;
